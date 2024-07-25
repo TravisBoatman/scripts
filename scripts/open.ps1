6fdf4d52d4c67ec1af4223cb3bab990b218456e9
Travis Boatman <Travis.Boatman@relativity.com>
2024-07-25 09:48:57 -0700

    Add support for running CLI commands

--- scripts/open.ps1
+++ scripts/open.ps1
@@ -12,6 +12,14 @@ function Expand-PathVariables($path) {
     return $ExecutionContext.InvokeCommand.ExpandString($path)
 }
 
+function Is-Url($string) {
+    return $string -match "^https?://"
+}
+
+function Is-Path($string) {
+    return Test-Path $string
+}
+
 try {
     $jsonPath = [System.Environment]::ExpandEnvironmentVariables("$env:TB_PATHS\actions.json")
     $actions = Get-Content -Path $jsonPath -ErrorAction Stop | ConvertFrom-Json
@@ -28,7 +36,17 @@ if ($names -contains "help") {
 foreach ($name in $names) {
     if ($actions.PSObject.Properties.Name -contains $name) {
         $resolvedPath = Expand-PathVariables $actions.$name
-        Start-Process $resolvedPath
+        
+        if (Is-Url $resolvedPath) {
+            Start-Process -FilePath $resolvedPath
+        } elseif (Is-Path $resolvedPath) {
+            Start-Process -FilePath $resolvedPath
+        } else {
+            $parts = $resolvedPath -split ' ', 2
+            $exe = $parts[0]
+            $args1 = if ($parts.Count -gt 1) { $parts[1] } else { $null }
+            Start-Process -FilePath $exe -ArgumentList $args1
+        }
     } else {
         Write-Host "Unknown option: $name"
         Show-Options
